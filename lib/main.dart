1e5b60ab0466bba4bc25f1e03fb8a2858008d868
TaigaTinouchi <tiger-0724h10@outlook.com>
2025-01-19 17:10:36 +0900

    backgroundセットアップ

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
-import 'package:p2p_test/riverpod/firebase/firebase_app/app.dart';
-import 'package:p2p_test/riverpod/theme/theme.dart';
+import 'package:flutter_background/flutter_background.dart';
+import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+
 import 'package:p2p_test/ui/core/ui_exporter.dart';
+import 'package:p2p_test/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (WebRTC.platformIsDesktop) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  } else if (WebRTC.platformIsAndroid) {
+    startForegroundService();
+  }
   runApp(
     UncontrolledProviderScope(
       container: ProviderContainer(),
@@ -12,40 +20,16 @@ Future<void> main() async {
   );
 }
 
-class App extends ConsumerWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final themeMode = ref.watch(themeModeProvider);
-    final router = ref.watch(routerProvider);
-    return ref.watch(fbAppProvider).when(
-      data: (app) => MaterialApp.router(
-        title: 'P2P Test',
-        theme: ThemeData.light(),
-        darkTheme: ThemeData.dark(),
-        themeMode: themeMode,
-        routerConfig: router,
-        builder: (context, child) {
-          // 現在のMediaQueryデータを取得
-          final mediaQueryData = MediaQuery.of(context);
-          // テキストスケールファクターを制限
-          final clampedTextScaler = mediaQueryData.textScaler.clamp(
-            minScaleFactor: 0.8,
-            maxScaleFactor: 1.0,
-          );
-          if (child == null) {
-            return const SizedBox();
-          }
-          // 新しいMediaQueryDataを作成して適用
-          return MediaQuery(
-            data: mediaQueryData.copyWith(textScaler: clampedTextScaler),
-            child: child,
-          );
-        },
-      ),
-      loading: () => const Center(child: CircularProgressIndicator()),
-      error: (error, _) => Center(child: Text('Error: $error')),
-    );
-  }
+Future<bool> startForegroundService() async {
+  final androidConfig = FlutterBackgroundAndroidConfig(
+    notificationTitle: 'Title of the notification',
+    notificationText: 'Text of the notification',
+    notificationImportance: AndroidNotificationImportance.normal,
+    notificationIcon: AndroidResource(
+      name: 'background_icon',
+      defType: 'drawable'
+    ),
+  );
+  await FlutterBackground.initialize(androidConfig: androidConfig);
+  return FlutterBackground.enableBackgroundExecution();
 }
